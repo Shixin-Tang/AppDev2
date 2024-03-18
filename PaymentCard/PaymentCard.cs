9294afcec6bf04c6041c3bf257a4d92bb785d739
ShixinTang <shixin.tang@outlook.com>
2024-03-17 22:17:39 -0400

    Add PaymentCard project

---
+++ PaymentCard/PaymentCard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentCard
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            this.balance = openingBalance;
+        }
+        public override string ToString()
+        {
+            return "The card has a balance of " + this.balance + " euros";
+           
+        }
+
+        public void EatLunch()
+        {
+            if (this.balance >= 10.60)
+            {
+                this.balance = this.balance - 10.60;
+            }
+        }   
+
+        public void DrinkCoffee()
+        {
+            if (this.balance >= 2.0)
+            {
+                this.balance = this.balance - 2.0;
+            }
+        }
+
+        public void AddMoney(double amount)
+        {
+            if (amount < 0 )
+            {
+                return;
+            }
+
+            this.balance = this.balance + amount;
+    
+            if (this.balance > 150)
+            {
+                this.balance = 150;
+            }
+        }
+    }
+}
---
+++ PaymentCard/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentCard card = new PaymentCard(100);
+            Console.WriteLine(card);
+
+            card.AddMoney(49.99);
+            Console.WriteLine(card);
+
+            card.AddMoney(10000.0);
+            Console.WriteLine(card);
+
+            card.AddMoney(-10);
+            Console.WriteLine(card);
+        }
+    }
+}
